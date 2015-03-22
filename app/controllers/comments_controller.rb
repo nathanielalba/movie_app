cffeb0eee53ac949cfeb88988395643658242880
Nathaniel Alba <nathaniel.alba@gmail.com>
2015-03-22 16:17:59 -0700

    add users to comments, and finish up search bar for multiple models

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,8 @@ class CommentsController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-		@comment = @commentable.comments.new comment_params
-		@comment.user = current_user
-		@comment.save
+		@comment = @commentable.comments.new(comment_params)
+		@comment.user = current_user.email
 
 		if @comment.save
 			redirect_to @commentable, notice: "Your comment was successfully posted."
